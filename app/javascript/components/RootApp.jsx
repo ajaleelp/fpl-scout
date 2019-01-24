4254a3ad323eb48f610ca6b51bd4c296ed9a7506
Abdul Jaleel <ajaleelp@gmail.com>
2019-01-25 03:51:25 +0530

    Convert cost slider into a range slider

--- app/javascript/components/RootApp.jsx
+++ app/javascript/components/RootApp.jsx
@@ -1,6 +1,7 @@
 import React from "react"
 import PropTypes from "prop-types"
 import Slider from 'rc-slider';
+const Range = Slider.Range;
 import 'rc-slider/assets/index.css';
 import PlayerCarousel from "./PlayerCarousel";
 
@@ -9,29 +10,49 @@ export default class RootApp extends React.Component {
         super(props);
 
         this.state = {
-            maxCost: 150,
+            maxCost: this.globalMaxCost(),
+            minCost: this.globalMinCost(),
             players: this.props.players
         };
 
         this.filteredPlayers = this.filteredPlayers.bind(this);
-        this.changeMaxCost = this.changeMaxCost.bind(this);
+        this.changeRange = this.changeRange.bind(this);
+    }
+
+    globalMaxCost() {
+        return Math.max(...this.props.players.map((player) => {return player.cost}));
+    }
+
+    globalMinCost() {
+        return Math.min(...this.props.players.map((player) => {return player.cost}));
     }
 
     filteredPlayers() {
-        return this.state.players.filter((player) => {return parseInt(player.cost) < this.state.maxCost;});
+        return this.state.players.filter((player) => {
+            return ((player.cost <= this.state.maxCost) && (player.cost >= this.state.minCost));
+        });
     }
 
-    changeMaxCost(newMaxCost) {
-        console.log(newMaxCost);
-        this.setState({maxCost: newMaxCost});
+    changeRange([newMinCost, newMaxCost]) {
+        this.setState({minCost: newMinCost, maxCost: newMaxCost});
     }
 
     render() {
+        let sliderMarks = {};
+        sliderMarks[this.globalMinCost()] = this.globalMinCost().toString();
+        sliderMarks[this.globalMaxCost()] = this.globalMaxCost().toString();
         return (
             <div className="row">
                 <div className="col-md-2"></div>
                 <div className="col-md-8 root-container px-4">
-                    <Slider className="slider-root mx-auto" min={0} max={150} marks={{0: '0', 150: '150'}} onAfterChange={this.changeMaxCost}/>
+                    <Range className="slider-root mx-auto"
+                            min={this.globalMinCost()}
+                            max={this.globalMaxCost()}
+                            marks={sliderMarks}
+                            onChange={this.changeRange}
+                            value={[this.state.minCost, this.state.maxCost]}
+                            allowCross={false}
+                    />
                     <PlayerCarousel players={this.filteredPlayers()}/>
                 </div>
                 <div className="col-md-2"></div>
